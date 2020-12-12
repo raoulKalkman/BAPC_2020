23287ee937e04c37156627e5ab8f94fcc505b4d3
Raoul Kalkman <raoulkalkman@outlook.com>
2020-12-12 16:46:12 +0100

    write pseudocode for D

--- D/solution.py
+++ D/solution.py
@@ -1,6 +1,13 @@
-w, h = [int(a) for a in input().split()]
+w, h = [int(a) for a in input().split()]    #h = height, w = width
+
+#for x-axis binary search -> find either horizon or 2 pixels where sky > sea
+# (recursive)
+#   with found horizon (can be float) look at pixels y, y+1, y-1, y+2, y-2,.... -> check if horizon is found here
+#   when valid horizon pixel is found (not float) store in points
+#   when points has 2 points -> return
 
 points = []
+column = [] #for x axis
 for x in range(1, w+1):
     if len(points) == 2:
         break
@@ -8,10 +15,19 @@ for x in range(1, w+1):
     while lim[0] <= lim[1]:
         mid = int((lim[0] + lim[1]) / 2)
         print('?', x, mid, flush=True)
-        res = input()
+        res = input() #get input for mid
+        column[mid] = res
+
         if res == 'sea': # too low
+            if column[mid + 1] == 'sky': #horizon between mid & mid + 1
+
+
             lim[0] = mid+1
         elif res == 'sky': # too high
+            if column[mid - 1] == 'sea':
+
+
+
             lim[1] = mid-1
         else:
             points.append((x, mid))
