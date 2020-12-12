cb37e45c9f01400d9598dd302e3dddbb9acbc568
Raoul Kalkman <raoulkalkman@outlook.com>
2020-12-12 10:51:48 +0100

    update boundaries

--- Y/solution.py
+++ Y/solution.py
@@ -4,10 +4,10 @@ import math
 
 fieldSize = 10
 
-userInput = float(input("input a number between -75 and 75 "))
+userInput = float(input())
 
 #not necessary
-while userInput > 75.0 or userInput < -75.0:
+while userInput >= 75.0 or userInput <= -75.0:
     print("invalid input, try again.")
     userInput = float(input("input a number between -75 and 75 "))
 
@@ -15,4 +15,3 @@ userInput = math.radians(userInput)
 distance = fieldSize*math.tan(userInput)
 
 print(distance, flush=True)
-
