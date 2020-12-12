601bd2d5904c52515f9479b3a3553b88bbba823a
Raoul Kalkman <raoulkalkman@outlook.com>
2020-12-12 13:32:21 +0100

    allow C to take file as input

--- C/solution.py
+++ C/solution.py
@@ -1,5 +1,13 @@
-n, p = [int(a) for a in input().split(' ')] # teams, problems - 1e4
-rankings = [int(input()) for i in range(n)] # ranking, time score - 1e6
+import sys
+if len(sys.argv) > 1:
+    with open(sys.argv[1], 'r') as Userinput:
+        line = Userinput.readline()
+        n, p = [int(a) for a in line.split(' ')]
+        rankings = [int(Userinput.readline()) for i in range(n)]
+        Userinput.close()
+else:
+    n, p = [int(a) for a in input().split(' ')] # teams, problems - 1e4
+    rankings = [int(input()) for i in range(n)] # ranking, time score - 1e6
 
 # Que: Find how many problems each team has solved
 # Sol: 0 time penalty means that 0 problems have been solved, when a low number follows a high one a higher numbers of solutions has been found (to explain the ranking)
